2d8bb6d7d1193815e20bc7c99c812867bf9c811f
TheDestroyer55e <156495253+TheDestroyer55e@users.noreply.github.com>
2026-05-31 14:31:16 -0400

    Add files via upload

---
+++ data/ui_scripts/string_override/__init__.lua
@@ -0,0 +1,18 @@
+if (game:issingleplayer() or not Engine.InFrontend()) then
+    return
+end
+
+game:addlocalizedstring("LUA_MENU_MP_SQUADS_DLC_CATEGORY_4_CAPS", "^3SAFEGUARD - BOTS^7");
+game:addlocalizedstring("ALIENS_CONTROLS_STRIKE_PACKAGE", "Strike Package");
+game:addlocalizedstring("ALIENS_CONTROLS_TEAM_AMMO", "Team Ammo");
+game:addlocalizedstring("ALIENS_CONTROLS_TEAM_SUPPORT", "Team Support");
+game:addlocalizedstring("ALIENS_CONTROLS_EQUALIZER", "Equalizer");
+game:addlocalizedstring("ALIENS_CONTROLS_TEAM_SUPPORT2", "Secondary Team Support");
+game:addlocalizedstring("ALIENS_TEAM_ADRENALINE_DESC", "Team boosters for heightened vision and hearing. Highlights enemies for 15 sec.");
+game:addlocalizedstring("ALIENS_TEAM_ADRENALINE_DESC_1", "Highlights enemies through walls.");
+game:addlocalizedstring("ALIENS_TEAM_ADRENALINE_DESC_1", "Regenerate Health Faster. Increases duration to 30 sec.");
+game:addlocalizedstring("ALIENS_TEAM_ADRENALINE_DESC_3", "Increases the booster's duration to 45 sec.");
+game:addlocalizedstring("ALIENS_TEAM_ADRENALINE_DESC_4", "Increases the booster's duration to 60 sec and further improves health regeneration.");
+game:addlocalizedstring("EXTRA_SUPPORT_DESC", "Allows you to choose a second Team Support item.");
+game:addlocalizedstring("EXTRA_SUPPORT", "Extra Support");
+game:addlocalizedstring("LUA_MENU_MP_SQUADS_DLC_DESC_4", "Play Safeguard alongside AI teammates.");
